35ecea1fe699336459f475095e4604bdc2011fb3
p0werfull321 <136437568+p0werfull321@users.noreply.github.com>
2023-10-06 13:24:29 +0530

    Update pipeline.py

--- vars/pipeline.py
+++ vars/pipeline.py
@@ -2,7 +2,7 @@ import requests
 
 # Define your Artifactory URL and API key or username/password
 artifactory_url = 'http://13.52.185.193:8082/artifactor'
-api_key = 'artifactory-access-token'
+api_key = 'AKCp8pRFHLkT21EPT8gvh3y5SouE5qDMK4ZXpv9pcNftwYj12WZssRq5QbRcgWL6whi4Xgmnc'
 repo_name = 'admin'
 
 # Upload a JAR file to Artifactory
@@ -10,7 +10,7 @@ jar_file_path = '/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.
 upload_url = f'http://13.52.185.193:8082/artifactory/example-repo-local/your/jar/file.jar'
 
 headers = {
-    'X-JFrog-Art-Api': artifactory-access-token,  # Use API key for authentication
+    'X-JFrog-Art-Api': AKCp8pRFHLkT21EPT8gvh3y5SouE5qDMK4ZXpv9pcNftwYj12WZssRq5QbRcgWL6whi4Xgmnc,  # Use API key for authentication
 }
 
 with open(jar_file_path, 'rb') as jar_file:
