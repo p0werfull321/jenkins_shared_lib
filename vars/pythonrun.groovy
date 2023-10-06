d56f49a08efc0cba401145892e83c21f9d2e62bb
p0werfull321 <136437568+p0werfull321@users.noreply.github.com>
2023-10-06 13:18:47 +0530

    Update pythonrun.groovy

--- vars/pythonrun.groovy
+++ vars/pythonrun.groovy
@@ -1,16 +1,3 @@
-def runPythonScript(String pythonScriptPath, String apiKey) {
-    try {
-        // Create a list of command-line arguments to pass to the Python script
-        def commandLineArgs = ["python", pythonScriptPath, "--api-key", apiKey]
-
-        def process = commandLineArgs.execute()
-        process.waitFor()
-
-        println "Python script executed with exit code: ${process.exitValue()}"
-    } catch (Exception e) {
-        println "An error occurred: ${e.message}"
-    }
+def call(){
+    sh './pipeline.py'
 }
-
-
-runPythonScript(pythonScriptPath, apiKey)
